d2475ec6d62719c95ab9bb26e298ea6260b4c710
jedichen121 <chenjiyang1991@gmail.com>
2023-05-13 20:47:25 +0200

    Initial commit

---
+++ execute.c
@@ -0,0 +1,7 @@
+#include "parse.h"
+
+/* TODO: implement this */
+int run_pipeline(struct pipeline *p) {}
+
+/* TODO: implement this */
+int run_builtin(enum builtin_type builtin, char *builtin_arg) {}
